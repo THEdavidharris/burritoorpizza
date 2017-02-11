695d8225888fc9df455c224866133f5c8930c9f5
David Harris <dharr419@ford.com>
2017-02-11 18:13:32 -0500

    Slight refactor to add subfolders to Components/

--- src/Components/Locator/Locator.js
+++ src/Components/Locator/Locator.js
@@ -27,8 +27,8 @@ class Locator extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="Locator-intro">
+          Getting your location...
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Locator from './Components/Locator';
+import Locator from './Components/Locator/Locator';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
